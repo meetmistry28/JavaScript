202b92f8c8d1be63a3f634716e8b4a233985a893
meetmistry28 <meetmistry7007@gmail.com>
2023-09-02 11:20:12 +0530

    cal_rad

--- js/cal.js
+++ js/cal.js
@@ -1,27 +1,39 @@
-let x = parseInt(prompt("Enter Value Of X : "));
-let y = parseInt(prompt("Enter Value Of Y : "));
+// let x = parseInt(prompt("Enter Value Of X : "));
+// let y = parseInt(prompt("Enter Value Of Y : "));
 
-let op = prompt("ENTER OPERATOR");
-// console.log(x,y,op);
+// let op = prompt("ENTER OPERATOR");
+// // console.log(x,y,op);
 
-let ans;
+// let ans;
 
-switch (op) {
-    case '+':
-        ans = x + y;
-        break;
+// switch (op) {
+//     case '+':
+//         ans = x + y;
+//         break;
 
-    case '-':
-        ans = x - y;
-        break
+//     case '-':
+//         ans = x - y;
+//         break
 
-    case '*':
-        ans = x * y;
-        break
+//     case '*':
+//         ans = x * y;
+//         break
     
-    case '/':
-        ans = x / y;
-        break
-}
+//     case '/':
+//         ans = x / y;
+//         break
+// }
 
-console.log(ans);
+// console.log(ans);
+
+function handelsubmit() {
+    // console.log("ok");
+    let r = parseFloat(document.getElementById("radius").value);
+
+    let area;
+
+    area = 3.14 * r * r;
+
+    console.log("Area Of circle is: ",area); 
+
+}
