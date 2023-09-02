40fbc9ed1b620741a93a4c180b2ac1e510c27b73
meetmistry28 <meetmistry7007@gmail.com>
2023-09-02 10:17:59 +0530

    02.09var

---
+++ js/variable.js
@@ -0,0 +1,60 @@
+// VAR 
+
+// 1.
+
+// var x = 10;
+// var x = 20;
+// console.log(x);
+
+// 2. 
+
+// var x;
+// console.log(x);
+
+// 3.
+
+// var x = 100;
+// var x;
+// console.log(x);
+
+
+// 4.
+
+// var x = 10;
+
+// variables have no block scop
+
+// {
+//     //block scope
+//     var x = 30;
+// }
+// console.log(x)
+
+// LET
+
+// 1.
+
+// let x = 10;
+// let x = 20;
+// console.log(x); // error
+
+// 2.
+
+// let x = 10;
+
+// {
+//     let x = 20;
+//     console.log(x);
+// }
+// console.log(x); // 10
+
+// 3.
+
+// let x;
+// x = 10;
+// x = x + 5;
+
+// console.log(x); // 15
+
+
+
