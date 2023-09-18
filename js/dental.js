27119b6d901de6b90fb1e45f61e082d2253d5185
yashbhalani8866 <yashbhalani8866@gmail.com>
2023-09-18 12:18:59 +0530

    dental

--- js/dental.js
+++ js/dental.js
@@ -1,3 +1,9 @@
-function handlTreatment() {
-    console.log("ok");
-}
+// function handlTreatment() {
+//     console.log("ok");
+// }
+
+let TC_s=1, TC_c=500;
+let F_s=2, F_c=1500;
+let RCT_s=4, RCT_c=2500;
+let RCT_C_s=6, RCT_C_c=12000;
+let BI_s=12, BI_c=35000;
