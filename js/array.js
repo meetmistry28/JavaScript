db3b6871979eb624890894ce95842943a2992fd3
yashbhalani8866 <yashbhalani8866@gmail.com>
2023-09-21 13:17:05 +0530

    arraytask

---
+++ js/array.js
@@ -0,0 +1,40 @@
+// 1. Creating an Array
+
+// const cars = ["alto", "funty", "van"];
+
+// console.log(cars[2]);
+
+
+
+
+
+// 2. Change Index
+
+// const cars = ["alto", "funty", "van"];
+
+// console.log(cars[0] = "BMW");
+
+
+
+// 3. Full Array
+
+// const bikes = ["Splender","BMW","Shine"];
+
+// console.log(bikes);
+
+
+
+// 4. Converting an Array to a String
+
+// const food = ["Pizza","Noodels","Idli","Coco"];
+
+// console.log(food.toString());
+
+
+
+// 5. Arrays are Objects
+
+// const person = {firstName:"John", lastName:"Doe", age:46};
+
+// console.log(person);
+
