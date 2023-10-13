5e28c5c9fcedadb51992530844b13203031b2e82
yashbhalani007 <yashbhalani007@gmail.com>
2023-10-13 11:26:33 +0530

    budget

---
+++ js/budgetapp.js
@@ -0,0 +1,18 @@
+class Budget {
+    constructor () {
+
+    }
+
+    submitBudget() {
+        console.log("submit thaigyu la");
+    }
+
+    submitExpense() {
+        console.log("tara bo kharcha che bhai");
+    }
+}
+
+let b = new Budget();
+
+b.submitBudget()
+b.submitExpense()
